452495a28c7a999a1f60e89b68ba15750ed7373c
tkdjedi <tkdjedi@gmail.com>
2026-09-16 21:17:11 -0700

    first commit. Added an .gitignore

---
+++ Pomnie.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace::std;
+int main() {
+
+    cout << "How do I get out?" << endl;
+
+    cout << "I don't want a sun burn" << endl;
+
+    return 0;
+    
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace::std;
+int main() {
+
+    cout << "That's probably just magnets" << endl;
+    // cout << "I think I may have found a way out." << endl;
+
+    return 0;
+}
